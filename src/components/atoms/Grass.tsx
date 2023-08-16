6bbd82a8584191deaabe8c19945b9cfadfdef56c
ysknsid25 <kengo071225@gmail.com>
2023-08-16 09:46:29 +0900

    草コンポーネントのatoms
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ src/components/atoms/Grass.tsx
@@ -0,0 +1,27 @@
+//import {react} from "react"
+interface GrassProps {
+    /**
+     * GitHubのコントリビュート数
+     */
+    contributes: number
+}
+
+/**
+ * GitHubへのコントリビュート数の草
+ */
+export const Grass = (
+    { contributes }: GrassProps
+): JSX.Element => {
+    return (
+        <div className={[
+            'h-3 w-3',
+            'rounded-sm',
+            'border-solid border-2',
+            0 == contributes && 'border-gray-100 bg-gray-100',
+            0 <= contributes && contributes < 5 && 'border-green-400 bg-green-400',
+            5 <= contributes && contributes < 10 && 'border-green-500 bg-green-500',
+            10 <= contributes && contributes < 15 && 'border-green-600 bg-green-600',
+            15 <= contributes && 'border-green-700 bg-green-700',
+        ].join(" ")}></div>
+    )
+}
---
+++ src/stories/Grass.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Grass } from "../components/atoms/Grass";
+
+const meta = {
+    title: 'Example/Grass',
+    component: Grass,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+    },
+} satisfies Meta<typeof Grass>;
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const None: Story = {
+    args: {
+        contributes: 0,
+    },
+};
+
+export const Bit: Story = {
+    args: {
+        contributes: 3,
+    },
+};
+
+export const Decent: Story = {
+    args: {
+        contributes: 6,
+    },
+};
+
+export const Often: Story = {
+    args: {
+        contributes: 11,
+    },
+};
+
+export const TooMany: Story = {
+    args: {
+        contributes: 20,
+    },
+};
